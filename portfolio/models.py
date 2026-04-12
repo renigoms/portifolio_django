1bcb74b1ec75df61ce4c740e28004e8d69d70ef6
renigoms <renan.nic28@gmail.com>
2026-04-12 18:46:09 -0300

    feat: create profile, certificate and project models

--- portfolio/models.py
+++ portfolio/models.py
@@ -1,3 +1,21 @@
 from django.db import models
 
 # Create your models here.
+class Certificate(models.Model):
+    description = models.CharField('Descrição', max_length=255, blank=False, null=False)
+
+class Project(models.Model):
+    class ProjectTypeChoice(models.TextChoices):
+        PERSONAL = 'PESSOAL', 'Pessoal'
+        SUBJECT = 'DISCIPLINA', 'Disciplina'
+
+    project_type = models.CharField('Tipo',
+                                    choices=ProjectTypeChoice,
+                                    default=ProjectTypeChoice.PERSONAL,
+                                    max_length=10)
+    name = models.CharField('Nome', max_length=100, blank=False, null=False)
+    description = models.CharField('Descrição', max_length=255, blank=False, null=False)
+    git = models.CharField('Git', max_length=100, blank=False, null=False)
+
+
+
