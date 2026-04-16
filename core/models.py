86b3ea829226920e0c887ccd46e172792fbc4d99
renigoms <renan.nic28@gmail.com>
2026-04-15 21:46:36 -0300

    feat: Main part of the project completed

--- core/models.py
+++ core/models.py
@@ -4,11 +4,11 @@ from stdimage import StdImageField
 
 # Create your models here.
 class Profile(models.Model):
+    image_profile_url = StdImageField('Foto de Perfil', upload_to='images/profile/', variations={'thumb': (128, 128)})
     name = models.CharField('Nome', max_length=100, blank=False, null=False)
     email = models.EmailField('E-mail', max_length=100, blank=False, null=False)
     description = models.CharField("Sobre Mim", max_length=255, blank=False, null=False)
     course = models.CharField('Curso', max_length=100, blank=False, null=False)
+    period = models.CharField('Periodo', max_length=100, blank=False, null=False)
     linken = models.CharField('Linken', max_length=100, blank=False, null=False)
     git = models.CharField('Git', max_length=100, blank=False, null=False)
-    image_profile_url = StdImageField('Foto de Perfil', upload_to='images/profile/', variations={'thumb': (128, 128)})
-    period = models.CharField('Periodo', max_length=100, blank=False, null=False)
