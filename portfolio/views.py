bbfcb7338c6647d5ecc928a3e212fb5fb669fbea
renigoms <renan.nic28@gmail.com>
2026-04-18 22:01:03 -0300

    feat: pagina contato finalizada

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,7 +1,10 @@
+from django.contrib import messages
 from django.db.models import QuerySet
+from django.http import HttpResponse
 from django.shortcuts import render
 
 from core.models import Profile
+from portfolio.form import ContactForm
 from portfolio.models import Certificate, Project
 
 
@@ -15,3 +18,18 @@ def projetos(request):
     projects: QuerySet[Project] = Project.objects.all()
     return render(request, 'portfolio/projetos.html',
                   {'projects': projects, 'ProjectType': Project.ProjectTypeChoice})
+
+def contato(request):
+    form = ContactForm(request.POST or None)
+    if str(request.method) == 'POST':
+        if form.is_valid():
+            form.send_email()
+            # Isso é o que vai fazer o {% bootstrap_messages %} funcionar
+            messages.success(request, 'E-mail enviado com sucesso!')
+            # form = ContactForm(request.POST or None)
+            form = ContactForm()
+        else:
+            messages.error(request, 'Erro ao enviar a mensagem!')
+    return render(request,
+                  'portfolio/contato.html',
+                  {'form': form})
